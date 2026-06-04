2106e5e4364ade639bed9332e249488ed9137933
Lama <lamamqqz@gmail.com>
2026-06-04 17:42:01 +0300

    fix: output error

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i) for i in range(97, 123) if chr(i) not in "qe")))
+print("{}".format("".join(chr(i) for i in range(97, 123) if chr(i) not in "qe")), end="")
