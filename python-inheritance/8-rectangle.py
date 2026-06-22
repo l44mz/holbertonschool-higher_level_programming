c4c17cf39cff4615e51b8e27cdcd4039f002cbc0
Lama <lamamqqz@gmail.com>
2026-06-22 17:12:35 +0300

    add: task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class that represents a rectangle, inheriting from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a Rectangle with validated width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
