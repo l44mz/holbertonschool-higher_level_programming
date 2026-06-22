d11bf255203eb8ae78ae17cea817f3808dab2744
Lama <lamamqqz@gmail.com>
2026-06-22 16:19:14 +0300

    add: task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a class MyList that inherits from list."""
+
+
+class MyList(list):
+    """A class that inherits from list and adds a sorted print method."""
+
+    def print_sorted(self):
+        """Print the list elements sorted in ascending order without modifying the original list."""
+        print(sorted(self))
