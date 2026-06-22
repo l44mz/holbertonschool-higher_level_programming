2026f1cc514d98e1a02a497165e57357029a469b
Lama <lamamqqz@gmail.com>
2026-06-22 17:16:23 +0300

    add: task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines a Square class that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class that represents a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize a Square with a validated size.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size
