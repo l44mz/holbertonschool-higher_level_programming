b62e0602b96760358c7ccdc337e5e21140f9d25c
Lama <lamamqqz@gmail.com>
2026-07-21 15:55:35 +0300

    fix: added binary

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,7 +19,9 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
